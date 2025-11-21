f7e044524cf3deb5b40739a327bf3f2769cbb678
kapufskipauw <rosarhode@gmail.com>
2025-11-21 19:39:46 +0100

    chore: Add pseudo program

--- index.js
+++ index.js
@@ -3,3 +3,20 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+// create an inputfield 
+// create a button that activates a onclick function
+
+// write html.textcontent to output inputfield value to UI in all blocks 
+
+// create a variable that takes given value from inputfield 
+
+// write a function for length
+// that takes inputfield value multiplied by 3.281 feet
+
+// write a function for volume 
+// that takes inputfield value multiplied by 0.264 gallon
+
+// write a function for mass
+// that takes inputfield value multiplied by 2.204 pound
+
