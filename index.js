5ea51f844b00da56cfd4a38db9a37a67a40875b3
kapufskipauw <rosarhode@gmail.com>
2025-11-22 20:26:53 +0100

    end: Commented old code
    So I can keep them to visit later on

--- index.js
+++ index.js
@@ -5,58 +5,72 @@ let sumVolumeEl = document.getElementById("sum-volume-el")
 let sumLengthEl = document.getElementById("sum-length-el")
 let sumMassEl = document.getElementById("sum-mass-el")
 
-convertBtn.addEventListener( "click", btnClicked) 
 const meterToFeet = 3.281
 const literToGallon = 0.264 
 const kilogramToPount = 2.204
 
-    function btnClicked() {
-        console.log("Button is clicked, you are a pro!")
-        sumLength()
-        sumVolume()
-        sumMass()
-    }
 convertBtn.addEventListener("click", function(){ 
     let inputValue = inputEl.value
 
-    function sumLength(){
-        let inputValue = inputEl.value
-        let multiOutput = inputValue * 3.281
-        sumLengthEl.textContent = `${inputValue} meters = ${multiOutput.toFixed(2)} feet | ${multiOutput.toFixed(2)} feet = ${inputValue} meters`
-    }
     sumLengthEl.textContent = `${inputValue} meters = ${inputValue * meterToFeet.toFixed(2)} feet | 
     ${inputValue * meterToFeet.toFixed(2)} feet = ${inputValue} meters`
 
-    function sumVolume(){
-        let inputValue = inputEl.value
-        let multiOutput = inputValue * 0.264
-        sumVolumeEl.textContent = `${inputValue} liters = ${multiOutput.toFixed(2)} gallons | ${multiOutput.toFixed(2)} gallons = ${inputValue} liters`
-    }
     sumVolumeEl.textContent = `${inputValue} liters = ${inputValue * literToGallon.toFixed(2)} gallons | 
     ${inputValue * literToGallon.toFixed(2)} gallons = ${inputValue} liters`
 
-    function sumMass(){
-        let inputValue = inputEl.value
-        let multiOutput = inputValue * 2.204
-        sumMassEl.textContent = `${inputValue} kilos = ${multiOutput.toFixed(2)} pounds | ${multiOutput.toFixed(2)} pounds = ${inputValue} kilos`
-    }
-// create an inputfield 
-// create a button that activates a onclick function
     sumMassEl.textContent = `${inputValue} kilos = ${inputValue * kilogramToPount.toFixed(2)} pounds | 
     ${inputValue * kilogramToPount.toFixed(2)} pounds = ${inputValue} kilos`
 }
 )
 
-// write html.textcontent to output inputfield value to UI in all blocks 
+// convertBtn.addEventListener( "click", btnClicked) 
 
-// create a variable that takes given value from inputfield 
+//     function btnClicked() {
+//         console.log("Button is clicked, you are a pro!")
+//         sumLength()
+//         sumVolume()
+//         sumMass()
+//     }
 
-// write a function for length
-// that takes inputfield value multiplied by 3.281 feet
+//     function sumLength(){
+//         let inputValue = inputEl.value
+//         let multiOutput = inputValue * 3.281
+//         sumLengthEl.textContent = `${inputValue} meters = ${multiOutput.toFixed(2)} feet | ${multiOutput.toFixed(2)} feet = ${inputValue} meters`
+//     }
 
-// write a function for volume 
-// that takes inputfield value multiplied by 0.264 gallon
+//     function sumVolume(){
+//         let inputValue = inputEl.value
+//         let multiOutput = inputValue * 0.264
+//         sumVolumeEl.textContent = `${inputValue} liters = ${multiOutput.toFixed(2)} gallons | ${multiOutput.toFixed(2)} gallons = ${inputValue} liters`
+//     }
 
-// write a function for mass
-// that takes inputfield value multiplied by 2.204 pound
+//     function sumMass(){
+//         let inputValue = inputEl.value
+//         let multiOutput = inputValue * 2.204
+//         sumMassEl.textContent = `${inputValue} kilos = ${multiOutput.toFixed(2)} pounds | ${multiOutput.toFixed(2)} pounds = ${inputValue} kilos`
+//     }
+
+
+
+
+
+
+
+
+
+// // create an inputfield 
+// // create a button that activates a onclick function x
+
+// // write html.textcontent to output inputfield value to UI in all blocks 
+
+// // create a variable that takes given value from inputfield x
+
+// // write a function for length
+// // that takes inputfield value multiplied by 3.281 feet
+
+// // write a function for volume 
+// // that takes inputfield value multiplied by 0.264 gallon
+
+// // write a function for mass
+// // that takes inputfield value multiplied by 2.204 pound
 
